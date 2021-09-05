f3120559bb13735dcbe29e2eb843aca771491fa2
Alex Rauenzahn <xandermv@yahoo.com>
2021-09-05 23:46:17 +0000

    Created an array from the ground up.

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-const strings= ['a', 'b', 'c', 'd'];
-const numbers = [1,2,3,4,5];
-// Variable array is somewhere in memory and the computer knows it.
-// When I do array[2], i'm telling the computer, hey go to the array and grab the 3rd item from where the array is stored.
+class MyArray{
+	constructor(){
+		this.length = 0;
+		this.data = {};
+	}
+	get(index){
+return this.data[index];
+	}
 
+	push(item){
+		this.data[this.length] = item;
+		this.length++;
+		return this.length;
+	}
 
-//push
-strings.push('e'); //O(1)
+	pop(item){
+		const lastItem = this.data[this.length - 1];
+		delete this.data[this.length - 1];
+		this.legnth--;
+		return lastItem;
+	}
 
-//pop
-strings.pop();//O(1)
-strings.pop();//O(1)
+	delete(index){
+		const item = this.data[index];
+		this.shiftItems(index);
+	}
 
-//unshift
-strings.unshift('x')
+	shiftItems(index){
+		for(let i = index; i < this.length - 1; i++){
+			this.data[i] = this.data[i + 1];//adds the data that was at the adjacent index
+		}
+		delete this.data[this.length - 1];
+		this.length--;
+	}
+}
 
-//splice
-strings.splice(2, 0, 'alien');
-
-console.log(strings)
+const newArray = new MyArray();
+newArray.push('Hi');
+newArray.push('Hey');
+newArray.push('Hello');
+newArray.push('Good morning');
+newArray.push('Hola');
+newArray.delete(2);
+console.log(newArray);
